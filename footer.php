42ee9bfaeea44c4d9c369753acc815a9b731df99
Evrard de Visscher <evrarddevisscher@hotmail.com>
2019-06-25 19:21:43 +0200

    footer1st

--- footer.php
+++ footer.php
@@ -1,5 +1,21 @@
-
-	<!-- MAIN APP -->
+<section>
+  <div class="footergrey">
+    <div class="footergrey_container">
+      <div class="greyitem">
+        <p>adresse</p>
+      </div>
+      <div class="greyitem">
+        <p>social</p>
+      </div>
+      <div class="greyitem">
+        <p>contact</p>
+      </div>
+    </div>
+  </div>
+  <div class="footerblack">
+    <p class="logofooter">Le Sidecar</p>
+  </div>
+</section>	<!-- MAIN APP -->
 	<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); echo '/main.js?' . filemtime( get_stylesheet_directory() . '/main.js'); ?>"></script>
 	<?php wp_footer(); ?>
 </body>
