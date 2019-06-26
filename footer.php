b784628c80888f750878f9171404707f24d1a565
Evrard de Visscher <evrarddevisscher@hotmail.com>
2019-06-26 19:10:49 +0200

    footer2nd

--- footer.php
+++ footer.php
@@ -2,18 +2,30 @@
   <div class="footergrey">
     <div class="footergrey_container">
       <div class="greyitem">
-        <p>adresse</p>
+        <p>ADDRESS</p>
       </div>
       <div class="greyitem">
-        <p>social</p>
+        <div>
+          <p>FOLLOW US</p>
+        </div>
+        <div>
+          <a href="#" class="fa fa-facebook"></a>
+          <a href="#" class="fa fa-instagram"></a>
+          <a href="#" class="fa fa-envelope"></a>
+        </div>
       </div>
       <div class="greyitem">
-        <p>contact</p>
+        <p>CONTACT US</p>
       </div>
     </div>
   </div>
   <div class="footerblack">
-    <p class="logofooter">Le Sidecar</p>
+    <div class="footerblack_container">
+      <div class="imgfooter">
+       <img height="50px"src="<?php echo get_bloginfo('template_url') ?>/assets/images/SideCarLogo.svg"/>
+      </div>
+      <p class="logofooter">Le Sidecar</p>
+    </div>
   </div>
 </section>	<!-- MAIN APP -->
 	<script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); echo '/main.js?' . filemtime( get_stylesheet_directory() . '/main.js'); ?>"></script>
--- header.php
+++ header.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title><?php bloginfo('title'); ?></title>
-    
+
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="shortcut icon" href="favicon.ico"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); echo '/style.css?' . filemtime( get_stylesheet_directory() . '/style.css'); ?>">
 
     <?php wp_head(); ?>
