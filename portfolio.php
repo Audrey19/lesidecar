a75e24d0c653eb54c4d29cd965e3267f35907c48
audrey mertens <amertens@worldofdigits.com>
2019-07-02 14:54:35 +0200

    portfolio don

---
+++ portfolio.php
@@ -0,0 +1,88 @@
+<section class="portfolio padding-top--big">
+    <div class="portfolio_titles">
+        <h3 class="sectionTitle">- portfolio</h3>
+        <h1 class="firstTitle">Découvrez notre travail</h1>
+        <p class="paragraphe">Nous concevons votre site web de A à Z. Du design à la mise en ligne. <br> Nous transformons vos besoins en solution concrète.</p>
+    </div>
+
+    <div class="portfolio_flexbox margin-top--medium">
+        <div class="flexbox_item">
+            <img class="portfolio_image" src="<?php bloginfo('template_url'); ?>/assets/images/project1.png" alt="logo">
+            <div class="hover_box fade-in">
+                <div class="hover_text">
+                    <h1>Take A Mini Break</h1>
+                    <h2>SITE web sure mesure</h2>
+                    <h3>It picks up the words on the page and displays ads that are similar to those words. Then when someone either performs an action or clicks on your page you will get paid.</h3>
+                    <a class="btn btn_white" href="">Voir le site</a>
+                </div>
+            </div>
+        </div>
+        <div class="flexbox_item">
+            <img class="portfolio_image" src="<?php bloginfo('template_url'); ?>/assets/images/project1.png" alt="logo">
+            <div class="hover_box fade-in">
+                <div class="hover_text">
+                    <h1>Take A Mini Break</h1>
+                    <h2>SITE web sure mesure</h2>
+                    <h3>It picks up the words on the page and displays ads that are similar to those words. Then when someone either performs an action or clicks on your page you will get paid.</h3>
+                    <a class="btn btn_white" href="">Voir le site</a>
+                </div>
+            </div>
+        </div>
+        <div class="flexbox_item">
+            <img class="portfolio_image" src="<?php bloginfo('template_url'); ?>/assets/images/project1.png" alt="logo">
+            <div class="hover_box fade-in">
+                <div class="hover_text">
+                    <h1>Take A Mini Break</h1>
+                    <h2>SITE web sure mesure</h2>
+                    <h3>It picks up the words on the page and displays ads that are similar to those words. Then when someone either performs an action or clicks on your page you will get paid.</h3>
+                    <a class="btn btn_white" href="">Voir le site</a>
+                </div>
+            </div>
+        </div>
+        <div class="flexbox_item">
+            <img class="portfolio_image" src="<?php bloginfo('template_url'); ?>/assets/images/project1.png" alt="logo">
+            <div class="hover_box fade-in">
+                <div class="hover_text">
+                    <h1>Take A Mini Break</h1>
+                    <h2>SITE web sure mesure</h2>
+                    <h3>It picks up the words on the page and displays ads that are similar to those words. Then when someone either performs an action or clicks on your page you will get paid.</h3>
+                    <a class="btn btn_white" href="">Voir le site</a>
+                </div>
+            </div>
+        </div>
+        <div class="flexbox_item">
+            <img class="portfolio_image" src="<?php bloginfo('template_url'); ?>/assets/images/project1.png" alt="logo">
+            <div class="hover_box fade-in">
+                <div class="hover_text">
+                    <h1>Take A Mini Break</h1>
+                    <h2>SITE web sure mesure</h2>
+                    <h3>It picks up the words on the page and displays ads that are similar to those words. Then when someone either performs an action or clicks on your page you will get paid.</h3>
+                    <a class="btn btn_white" href="">Voir le site</a>
+                </div>
+            </div>
+        </div>
+        <div class="flexbox_item">
+            <img class="portfolio_image" src="<?php bloginfo('template_url'); ?>/assets/images/project1.png" alt="logo">
+            <div class="hover_box fade-in">
+                <div class="hover_text">
+                    <h1>Take A Mini Break</h1>
+                    <h2>SITE web sure mesure</h2>
+                    <h3>It picks up the words on the page and displays ads that are similar to those words. Then when someone either performs an action or clicks on your page you will get paid.</h3>
+                    <a class="btn btn_white" href="">Voir le site</a>
+                </div>
+            </div>
+        </div>
+        <div class="flexbox_item">
+            <img class="portfolio_image" src="<?php bloginfo('template_url'); ?>/assets/images/project1.png" alt="logo">
+            <div class="hover_box fade-in">
+                <div class="hover_text">
+                    <h1>Take A Mini Break</h1>
+                    <h2>SITE web sure mesure</h2>
+                    <h3>It picks up the words on the page and displays ads that are similar to those words. Then when someone either performs an action or clicks on your page you will get paid.</h3>
+                    <a class="btn btn_white" href="">Voir le site</a>
+                </div>
+            </div>
+        </div>
+        <div class="flexbox_item"></div>  
+    </div>
+</section>
