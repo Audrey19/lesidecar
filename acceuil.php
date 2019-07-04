80ff1762d7b16adf3601d310b505878d502fda18
Evrard de Visscher <evrarddevisscher@hotmail.com>
2019-07-04 19:09:47 +0200

    responsiveaccueil

--- acceuil.php
+++ acceuil.php
@@ -1,6 +1,6 @@
 <section class="acceuil_wrapper">
     <div class="acceuil_box_left">
-        <div class="box_left_width margin-top--big">
+        <div class="box_left_width_item margin-top--big">
 
             <h1 class="hometitle">Besoin d’un site Web?</h1>
             <p class="paragraphe">Nous concevons votre site web de A à Z. Du design à la mise en ligne.<br> Nous transformons vos besoins en solution concrète.</p>
@@ -9,9 +9,9 @@
         </div>
     </div>
     <div class="acceuil_box_middle">
-        <img class="acceuil_box_middle" src="<?php bloginfo('template_url'); ?>/assets/images/Video.png" alt="video">
+        <img class="acceuil_box_middle_item" src="<?php bloginfo('template_url'); ?>/assets/images/Video.png" alt="video">
     </div>
     <div class="acceuil_box_right">
-        <img class="acceuil_box_right" src="<?php bloginfo('template_url'); ?>/assets/images/profile_picture.png" alt="profile">
+        <img class="acceuil_box_right_item" src="<?php bloginfo('template_url'); ?>/assets/images/profile_picture.png" alt="profile">
     </div>
 </section>
