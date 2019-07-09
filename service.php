baf1528d328f39289c9c19476597d36c064306bf
audrey mertens <amertens@worldofdigits.com>
2019-07-09 16:55:05 +0200

    wordpress till service done

--- service.php
+++ service.php
@@ -2,40 +2,27 @@
     <img class="triangle" src="<?php bloginfo('template_url'); ?>/assets/images/mask_triangle.png" alt="logo">
     <div class="service">
         <div class="service_titles">
-            <h3 class="sectionTitle">- services</h3>
-            <h1 class="firstTitle">Consultez notre liste de services</h1>
-            <p class="paragraphe">Nous aimons explorer de nouvelles choses, c’est pourquoi nous couvrons de nombreuses matières.</p>
+            <h3 class="sectionTitle"><?php the_field('service-section'); ?></h3>
+            <h1 class="firstTitle"><?php the_field('service-title'); ?></h1>
+            <?php the_field('service-textarea'); ?>
         </div>
 
         <div class="service_flexbox padding-top--medium padding-bottom--small">
+        <?php  while ( have_rows('repeteur-service') ) : the_row(); ?>   
             <div class="service_item">
                 <div class="service_item_content padding-bottom--small">
-                    <img src="<?php bloginfo('template_url'); ?>/assets/images/layout.png" alt="logo">
-                    <h2>prix $$</h2>
-                    <h1>Site web avec template</h1>
-                    <p>Nous vous accompagnons dans le dessin de son site pour qu’il corresponde exactement à vos besoins et qu’il soit à la mesure de vos ambissions. Nous avons décidé de coder le front-end en HTML, SCSS et JS ainssi qu ele back en PHP et JS. </p>
+                <?php $logo = get_sub_field('box-logo');
+                    if( !empty($logo) ): ?>
+                        <img class="portfolio_image" src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>"">
+                <?php endif; ?>
+                    <h2><?php the_sub_field('box-section'); ?></h2>
+                    <h1><?php the_sub_field('box-title'); ?></h1>
+                    <?php the_sub_field('box-text'); ?>
                     <a href="#contact" class="btn btn_blue margin-top--small">Demander ce service</a>
                 </div>
             </div>
-            <div class="service_item">
-                <div class="service_item_content padding-bottom--small">
-                    <img src="<?php bloginfo('template_url'); ?>/assets/images/graphic-design.png" alt="logo">
-                    <h2>prix $$</h2>
-                    <h1>Site web avec template</h1>
-                    <p>Nous vous accompagnons dans le dessin de son site pour qu’il corresponde exactement à vos besoins et qu’il soit à la mesure de vos ambissions. Nous avons décidé de coder le front-end en HTML, SCSS et JS ainssi qu ele back en PHP et JS. </p>
-                    <a href="#contact"class="btn btn_blue margin-top--small">Demander ce service</a>
-                </div>
-            </div>
-            <div class="service_item">
-                <div class="service_item_content padding-bottom--small">
-
-                    <img src="<?php bloginfo('template_url'); ?>/assets/images/computer.png" alt="logo">
-                    <h2>prix $$</h2>
-                    <h1>Site web avec template</h1>
-                    <p>Nous vous accompagnons dans le dessin de son site pour qu’il corresponde exactement à vos besoins et qu’il soit à la mesure de vos ambissions. Nous avons décidé de coder le front-end en HTML, SCSS et JS ainssi qu ele back en PHP et JS. </p>
-                    <a href="#contact" class="btn btn_blue margin-top--small">Demander ce service</a>
-                </div>
-            </div>
+        <?php endwhile;?>
+            
         </div>
     </div>
 </section>
