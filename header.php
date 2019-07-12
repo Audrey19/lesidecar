e32fcb8849636253cad6064b7c89eb2007409c29
audrey mertens <amertens@worldofdigits.com>
2019-07-12 11:39:03 +0200

    debuggg

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 </head>
 <body>
 <header>
-  <img class="logo_black" src="<?php bloginfo('template_url'); ?>/assets/images/logo_bl.png" alt="logo">
+  <img class="logo_black" src="<?php bloginfo('template_url'); ?>/assets/images/logo.png" alt="logo">
   <p class="logo_name">LE SIDECAR</p>
   <?php wp_nav_menu(array('container_class' => 'menu_container','theme_location' => 'primary' )); ?>   
 </header>
